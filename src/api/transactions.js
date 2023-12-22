7eaac0af14a06d8d7c5f4de915be5972f75b4f49
devitoDbug <davidOchiengy@gmail.com>
2023-12-22 12:23:24 +0300

    feat(hooks): add useFetchUser hook

--- src/api/transactions.js
+++ src/api/transactions.js
@@ -17,3 +17,12 @@ export const fetchFollowing = async (username) => {
     console.log(e.message);
   }
 };
+
+export const fetchUser = async (username) => {
+  try {
+    const { data } = await httpClient.get(`/${username}`);
+    return data;
+  } catch (e) {
+    console.log(e.message);
+  }
+};
