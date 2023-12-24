58d79db4f27e30d52191027df72237c9f6d009f7
devitoDbug <davidOchiengy@gmail.com>
2023-12-24 23:14:49 +0300

    style: Center arrow inside circle for navigation

--- src/components/MessageSection.jsx
+++ src/components/MessageSection.jsx
@@ -27,7 +27,7 @@ const MessageSection = () => {
   return (
     <div className="relative h-full w-full p-2 md:w-full">
       <button
-        className="absolute h-9 w-9 rounded-full bg-C_DarkBlue text-lg text-C_TextWhiteDull md:hidden"
+        className="absolute flex h-9 w-9 items-center justify-center rounded-full bg-C_DarkBlue text-lg text-C_TextWhiteDull md:hidden "
         onClick={scrollToContactSection}
       >
         <FontAwesomeIcon icon={faArrowLeft} />
