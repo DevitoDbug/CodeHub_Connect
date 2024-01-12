be554dcbb8bf20c34c03c619c194897623e0b326
devitoDbug <davidOchiengy@gmail.com>
2024-01-12 15:01:45 +0300

    refactor: migrate userChats CRUD operations to TypeScript

---
+++ src/firebase/userChats.ts
@@ -0,0 +1,78 @@
+//CRUD for the list of chats for a given user in firebase
+import {
+  doc,
+  getDoc,
+  serverTimestamp,
+  setDoc,
+  updateDoc,
+} from "firebase/firestore";
+import { db } from "../firebase";
+
+interface updateUserChatsParams {
+  userID: string;
+  combinedID: string;
+  otherUserID: string;
+  otherUserDisplayName: string;
+  otherUserNickName: string;
+  otherUserEmail: string;
+  otherUserPhotoURL: string;
+}
+
+interface updateLastMessageAndDateParams {
+  userID: string;
+  combinedID: string;
+  lastMessage: string;
+}
+
+//creat user in userChats as an empty object
+export const createUserChats = async (uid: string) => {
+  await setDoc(doc(db, "userChats", String(uid)), {});
+};
+
+//get userChats from firebase
+export const getUserChats = async (uid: string) => {
+  const docRef = doc(db, "userChats", String(uid));
+  const docSnap = await getDoc(docRef);
+  return docSnap.data();
+};
+
+//update userChats in firebase.
+export const updateUserChats = async ({
+  userID,
+  combinedID,
+  otherUserID,
+  otherUserDisplayName,
+  otherUserNickName,
+  otherUserEmail,
+  otherUserPhotoURL,
+}: updateUserChatsParams) => {
+  await updateDoc(doc(db, "userChats", String(userID)), {
+    [combinedID + ".userInfo"]: {
+      id: otherUserID,
+      name: otherUserDisplayName,
+      login: otherUserNickName,
+      email: otherUserEmail,
+      avatar_url: otherUserPhotoURL,
+    },
+    [combinedID + ".date"]: serverTimestamp(),
+  });
+};
+
+//UpdateLastMessage in userChats in firebase
+export const updateLastMessageAndDate = async ({
+  userID,
+  combinedID,
+  lastMessage,
+}: updateLastMessageAndDateParams) => {
+  await updateDoc(doc(db, "userChats", String(userID)), {
+    [combinedID + ".lastMessage"]: { text: lastMessage },
+    [combinedID + ".date"]: serverTimestamp(),
+  });
+};
+
+//Check if userChats exists in firebase
+export const doesUserChatsExist = async (id: string) => {
+  const docRef = doc(db, "userChats", id);
+  const docSnap = await getDoc(docRef);
+  return docSnap.exists();
+};
