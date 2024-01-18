a9377d392346660f4a48f0af8c3a7f45a5f84edc
devitoDbug <davidOchiengy@gmail.com>
2024-01-18 10:16:05 +0300

    Delete unnecessary files and configurations

--- src/api/http-client.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-const REACT_APP_BASE_URL = `https://api.github.com/users`;
-// Axios config
-export const httpClient = axios.create({
-  baseURL: REACT_APP_BASE_URL,
-});
